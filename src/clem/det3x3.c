241779ed240f6ac504682aee657d32e162dbd8a4
Philipp Harms <philipp.harms@googlemail.com>
2015-06-09 12:07:19 +0200

    Implement det3x3f naively

--- src/clem/det3x3.c
+++ src/clem/det3x3.c
@@ -1,5 +1,22 @@
 #include "det3x3.h"
 
 clem_error_t det3x3f(matrix3x3f_buffer_t matrices, float *result) {
-  return CLEM_INVALID_VALUE;
+  size_t n = matrix3x3f_buffer_get_size(matrices);
+  if (n == 0) {
+    return CLEM_SUCCESS;
+  } else if (result == NULL) {
+    return CLEM_INVALID_VALUE;
+  }
+
+  for (size_t k=0; k<n; k++) {
+    result[k] =
+      matrices.data[0][0][k] * matrices.data[1][1][k] * matrices.data[2][2][k] +
+      matrices.data[0][1][k] * matrices.data[1][2][k] * matrices.data[2][0][k] +
+      matrices.data[0][2][k] * matrices.data[1][0][k] * matrices.data[2][1][k] -
+      matrices.data[0][2][k] * matrices.data[1][1][k] * matrices.data[2][0][k] -
+      matrices.data[0][1][k] * matrices.data[1][0][k] * matrices.data[2][2][k] -
+      matrices.data[0][0][k] * matrices.data[1][2][k] * matrices.data[2][1][k];
+  }
+
+  return CLEM_SUCCESS;
 }
--- tests/det3x3f_test.c
+++ tests/det3x3f_test.c
@@ -18,3 +18,63 @@ unit_test_result_t det3x3f_test_01(char **result_message) {
 
   return TEST_PASSED;
 }
+
+unit_test_result_t det3x3f_test_02(char **result_message) {
+  clem_error_t err;
+
+  matrix3x3f_buffer_t matrices = matrix3x3f_buffer_create();
+
+  matrix3x3f_t matrix;
+  matrix3x3f_buffer_pushback_arr(&matrices, matrix);
+
+  err = det3x3f(matrices, NULL);
+
+  if (err != CLEM_INVALID_VALUE) {
+    CREATE_TEST_RESULT_MESSAGE(result_message, "An error occurred in %s:%d",
+                               __FILE__, __LINE__);
+    return TEST_FAILED;
+  }
+
+  return TEST_PASSED;
+}
+
+unit_test_result_t det3x3f_test_03(char **result_message) {
+  clem_error_t err;
+  matrix3x3f_buffer_t buffer;
+
+  matrix3x3f_t matrix = {
+    { 0x1.p+25,  0x1.p+0,  0x0.555554p+0 },
+    { 0x1.p+25,  0x1.p+0,  0x1.fffffep+0 },
+    { 0x1.p+0,   0,        0x1.p+0       }
+  };
+
+  // (0,0) and (0,1) minor should cancel, leaving (0,2) minor as the result
+  float testee = 0.f, reference = 0x1.aaaaaap+0;
+
+  buffer = matrix3x3f_buffer_create();
+
+  err = matrix3x3f_buffer_pushback_arr(&buffer, matrix);
+
+  if (err != CLEM_SUCCESS) {
+    CREATE_TEST_RESULT_MESSAGE(result_message, "An error occurred in %s:%d",
+                               __FILE__, __LINE__);
+    return TEST_FAILED;
+  }
+
+  err = det3x3f(buffer, &testee);
+
+  if (err != CLEM_SUCCESS) {
+    CREATE_TEST_RESULT_MESSAGE(result_message, "An error occurred in %s:%d",
+                               __FILE__, __LINE__);
+    return TEST_FAILED;
+  }
+
+  if (testee != reference) {
+    CREATE_TEST_RESULT_MESSAGE(result_message, "An error occurred in %s:%d:\n"
+                               " - %a != %a",
+                               __FILE__, __LINE__, testee, reference);
+    return TEST_FAILED;
+  }
+
+  return TEST_PASSED;
+}
