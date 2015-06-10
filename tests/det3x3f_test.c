ee2bfa09d7b1dd97448a11becabe06de8459909a
Philipp Harms <philipp.harms@gmail.com>
2015-06-10 13:36:57 +0200

    Add test case for loss of significance in multiply

--- tests/det3x3f_test.c
+++ tests/det3x3f_test.c
@@ -84,3 +84,52 @@ unit_test_result_t det3x3f_test_03(char **result_message) {
 
   return TEST_PASSED;
 }
+
+unit_test_result_t det3x3f_test_04(char **result_message) {
+  clem_error_t err;
+  matrix3x3f_buffer_t buffer = matrix3x3f_buffer_create();
+
+  matrix3x3f_t matrix = {
+    { 0x1.000004p+0, 0.f,          -0x1.00000cp+0},
+    { 1.f,           0x1.000004p+0, 0            },
+    { 0.f,           1.f,           0x1.000004p+0}
+  };
+
+  // product of main diagonal is 0x1.00000c00003000008p+0
+  // if 0x1.00000cp+0 is subtracted, it leaves 0.3000008p-40 = 0x1.800002p-43
+  float testee = 0.f, reference = 0x1.800002p-43;
+
+  err = matrix3x3f_buffer_pushback_arr(&buffer, matrix);
+
+  if (err != CLEM_SUCCESS) {
+    UNIT_TEST_CREATE_RESULT_MESSAGE(
+      result_message, "An error occurred in %s:%d", __FILE__, __LINE__);
+    return TEST_FAILED;
+  }
+
+  err = matrix3x3f_buffer_pushback_arr(&buffer, matrix);
+
+  if (err != CLEM_SUCCESS) {
+    UNIT_TEST_CREATE_RESULT_MESSAGE(
+      result_message, "An error occurred in %s:%d", __FILE__, __LINE__);
+    return TEST_FAILED;
+  }
+
+  err = det3x3f(buffer, &testee);
+
+  if (err != CLEM_SUCCESS) {
+    UNIT_TEST_CREATE_RESULT_MESSAGE(
+      result_message, "An error occurred in %s:%d", __FILE__, __LINE__);
+    return TEST_FAILED;
+  }
+
+  if (testee != reference) {
+    UNIT_TEST_CREATE_RESULT_MESSAGE(
+      result_message, "An error occurred in %s:%d:\n - %a != %a",
+      __FILE__, __LINE__, testee, reference);
+    return TEST_FAILED;
+  }
+
+  matrix3x3f_buffer_free(buffer);
+  return TEST_PASSED;
+}
